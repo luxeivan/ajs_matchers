ab2f4b6987039693c2512599395f7965e4be621e
luxeivan <luxeivan@gmail.com>
2022-03-27 17:38:51 +0300

    fix eslint

--- src/__tests__/test_app.js
+++ src/__tests__/test_app.js
@@ -1,15 +1,14 @@
 import appObject from '../app';
 
 test('Тестирование сортировки массива', () => {
-
   const result = appObject([
-    {name: 'мечник', health: 10},
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
   ]);
-    expect(result).toEqual([
-      {name: 'маг', health: 100},
-      {name: 'лучник', health: 80},
-      {name: 'мечник', health: 10},
-    ]);
+  expect(result).toEqual([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
 });
--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
 export default function appObject(arrGamer) {
-  return arrGamer.sort((a, b) => {
-    return b.health - a.health;
-  });
+  return arrGamer.sort((a, b) => b.health - a.health);
 }
